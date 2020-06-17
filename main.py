93fbb814396e5c24cfaf00f07249e8be9bfcf575
Ivan Raab <raabdev@gmail.com>
2020-06-17 18:25:49 -0300

    se creo el repositorio

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, url_for
+
+
+#CREAMOS UNA INSTANCIA
+app = Flask(__name__)
+
+#CREAMOS EL DECORADOR
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/turnos/')
+def turnos():
+    return render_template('turnos.html')
+
+@app.route('/turnos/asignacion_turnos/')
+def asignacion_turnos():
+    return render_template('asignacion_turnos.html')
+
+
+
+
+
+#CORREMOS LA APP
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
