d3eca8ef915dfa9987280dc0c3e8d248a76cc0c0
Ivan <ivan.raab21@gmail.com>
2020-08-21 13:00:46 -0300

    se avanzo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 import sqlite3 as sql
 
 
@@ -17,11 +17,45 @@ def pacientes():
     cur = con.cursor()
     cur.execute("select * from pacientes")
     pacientes = cur.fetchall()
+    con.close()
     return render_template('pacientes.html', pacientes = pacientes)
 
 @app.route('/agregar_paciente')
 def add_paciente():
     return render_template('agregar_paciente.html')
+
+@app.route('/modificar/<string:id>')
+def get_paciente(id):
+    con = sql.connect('database.db')
+    cur = con.cursor()
+    cur.execute(f"select * from pacientes where id = {id}")
+    data = cur.fetchall()
+    return render_template('editar_paciente.html', paciente = data[0])
+
+@app.route('/update/<string:id>', methods = ["POST"])
+def update_paciente(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        dni = request.form['dni']
+        social = request.form['social']
+        plan = request.form['plan']
+        direccion = request.form['direccion']
+        localidad = request.form['localidad'] 
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute(f"update pacientes set nombre={nombre} where id = {id}")
+        #, dni = {dni}, social = {social}, plan = {plan}, direccion = {direccion}, localidad = {localidad}
+        return redirect(url_for('pacientes'))
+
+
+@app.route('/borrar/<string:id>')
+def borrar(id):
+    con = sql.connect('database.db')
+    cur = con.cursor()
+    cur.execute(f"delete from pacientes where id = {id}")
+    con.commit()
+    con.close()
+    return redirect(url_for('pacientes'))
 
 @app.route('/add_record', methods = ["POST", "GET"])
 def addrec():
@@ -48,10 +82,26 @@ def turnos():
 @app.route('/turnos/asignacion_turnos/', methods = ["GET", "POST"])
 def asignacion_turnos():
     if request.method == "POST":
-        return "joya"
-    else:       
+        
+        #practica elegida
+        practica = request.form['servicio1']
+
+        #buscamos los pacientes
+        con = sql.connect('database.db')
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute("select * from pacientes")
+        pacientes = cur.fetchall()
+        con.close()
+
+        return render_template('filtrar_paciente.html', pacientes = pacientes, practica = practica)
+    else:
+
         return render_template('asignacion_turnos.html')
 
+@app.route('/confirm_turno', methods = ["POST", "GET"])
+def confirm_turno():
+    return render_template('filtrar_paciente.html')
 
 
 
